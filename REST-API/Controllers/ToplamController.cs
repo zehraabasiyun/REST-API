c6f34ea7cfb6042df1f7b4d7c1e33858c602abc5
zehra abasiyun <zehraabasiyun@gmail.com>
2024-11-07 12:47:46 +0300

    İki sayıyı topla eklendi

---
+++ REST-API/Controllers/ToplamController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace REST_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToplamController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetToplam(int a, int b)
+        {
+            int toplam = a + b;
+            return Ok(new { sonuc = toplam });
+        }
+    }
+}
